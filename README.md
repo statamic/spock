fda66b069d3dd608b6abbb4a6621834ba3e479ed
Jason Varga <jason@pixelfear.com>
2016-01-22 13:00:44 -0500

    Initial addon

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# spock
+# Spock
+## Perform commands when content has been published.
+
+> Who better to listen for a publish event than a commander of the enterprise with the biggest ears?
+
+This addon is useful for automatically committing content.
---
+++ addons/Spock/SpockListener.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Statamic\Addons\Spock;
+
+use Statamic\API\Path;
+use Statamic\API\User;
+use Statamic\API\Parse;
+use Statamic\Extend\Listener;
+use Symfony\Component\Process\Process;
+
+class SpockListener extends Listener
+{
+    /**
+     * @var Statamic\Contracts\Data\Data
+     */
+    private $data;
+
+    /**
+     * Spock has no trouble listening for these events with those ears.
+     *
+     * @var array
+     */
+    public $events = [
+        'cp.published' => 'run'
+    ];
+
+    /**
+     * Handle the event, run the command(s).
+     *
+     * @param Statamic\Contracts\Data\Data $data
+     * @return void
+     */
+    public function run($data)
+    {
+        $this->data = $data;
+
+        $process = new Process($this->commands());
+
+        $process->run();
+    }
+
+    /**
+     * Get the concat'ed commands
+     *
+     * @return string
+     */
+    private function commands()
+    {
+        $full_path = Path::assemble(root_path(), $this->data->path());
+
+        $data = $this->data->toArray();
+        $data['full_path'] = $full_path;
+        $data['committer'] = User::getCurrent()->toArray();
+
+        $commands = [];
+
+        foreach ($this->getConfig('commands', []) as $command) {
+            $commands[] = Parse::template($command, $data);
+        }
+
+        return join('; ', $commands);
+    }
+}
