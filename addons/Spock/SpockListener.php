c8c59f6918434b49c780a56a9b78d7bd63f63581
Jesse Leite <jesseleite@gmail.com>
2018-06-19 10:40:52 -0400

    Listen when asset folder is deleted.

--- addons/Spock/SpockListener.php
+++ addons/Spock/SpockListener.php
@@ -24,6 +24,7 @@ class SpockListener extends Listener
         \Statamic\Events\Data\AssetDeleted::class => 'run',
         \Statamic\Events\Data\AssetContainerSaved::class => 'run',
         \Statamic\Events\Data\AssetFolderSaved::class => 'run',
+        \Statamic\Events\Data\AssetFolderDeleted::class => 'run',
         \Statamic\Events\Data\SubmissionSaved::class => 'run',
     ];
 
