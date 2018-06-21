f13b425688d6cdf44c91990a73818204aeca40b2
Jesse Leite <jesseleite@gmail.com>
2018-06-21 11:04:10 -0400

    Listen when collections and taxonomies are deleted.

--- addons/Spock/SpockListener.php
+++ addons/Spock/SpockListener.php
@@ -29,6 +29,8 @@ class SpockListener extends Listener
         \Statamic\Events\Data\AssetFolderDeleted::class => 'run',
         \Statamic\Events\Data\FileUploaded::class => 'run',
         \Statamic\Events\Data\SubmissionSaved::class => 'run',
+        \Statamic\Events\Data\CollectionDeleted::class => 'run',
+        \Statamic\Events\Data\TaxonomyDeleted::class => 'run',
         \Statamic\Events\Data\FieldsetSaved::class => 'run',
         \Statamic\Events\Data\FieldsetDeleted::class => 'run',
         \Statamic\Events\Data\SettingsSaved::class => 'run',
