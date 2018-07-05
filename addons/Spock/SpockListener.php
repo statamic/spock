8501a106230499e2f92b7460bd6c8584e6b4e86c
Jason Varga <jason@pixelfear.com>
2018-07-05 12:46:49 -0400

    Listen for addon settings saved

--- addons/Spock/SpockListener.php
+++ addons/Spock/SpockListener.php
@@ -14,6 +14,7 @@ class SpockListener extends Listener
      * @var array
      */
     public $events = [
+        \Statamic\Events\Data\AddonSettingsSaved::class => 'run',
         \Statamic\Events\Data\AssetUploaded::class => 'run',
         \Statamic\Events\Data\AssetMoved::class => 'run',
         \Statamic\Events\Data\AssetDeleted::class => 'run',
